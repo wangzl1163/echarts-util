3a8fe6d5823c6781e38c9b3cc28aa2bc76604e50
wangzl1163 <wangzl1163@qq.com>
2023-05-04 15:54:31 +0800

    chore: 去除 standard eslint 规范

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,10 @@
 module.exports = {
    env: {
       node: true,
-      es2021: true
+      es2022: true
    },
-   extends: [
-      'eslint:recommended',
-      'standard',
-      'prettier'
-   ],
-   plugins: [
-      '@typescript-eslint'
-   ],
+   extends: ['eslint:recommended', 'prettier'],
+   plugins: ['@typescript-eslint'],
    parser: '@typescript-eslint/parser',
    rules: {
       indent: ['error', 3],
