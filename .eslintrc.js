50363a863db11b830d04763aa9125fe709077ebf
wangzl1163 <wangzl1163@qq.com>
2023-07-06 15:03:43 +0800

    chore: 修改代码风格配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,18 @@
 module.exports = {
+   root: true,
    env: {
       node: true,
-      es2022: true
+      es2021: true
    },
-   extends: ['eslint:recommended', 'prettier'],
+   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
    plugins: ['@typescript-eslint'],
    parser: '@typescript-eslint/parser',
    rules: {
       indent: ['error', 3],
       'no-tabs': ['error', { allowIndentationTabs: true }],
-      'prefer-promise-reject-errors': ['error', { allowEmptyReject: true }]
+      'prefer-promise-reject-errors': ['error', { allowEmptyReject: true }],
+      '@typescript-eslint/indent': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-empty-interface': 'off'
    }
 }
